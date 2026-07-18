1a13a13843e5c43c31f685ec077e28de590d35c9
Abhishek M Raikar <aabhishekmraikar@gmail.com>
2026-07-18 16:56:21 +0530

    Fix BrowserRouter import

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,25 @@
-import { Link } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import Home from "./pages/Home";
-import Report from "./pages/Report";
-import AdminReports from "./pages/AdminReports";
 import Recommend from "./pages/Recommend";
+import Report from "./pages/Report";
 import Dashboard from "./pages/Dashboard";
+import AdminReports from "./pages/AdminReports";
 import Login from "./pages/Login";
 
-export default function App() {
+function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/recommend" element={<Recommend />} />
-        <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/report" element={<Report />} />
+        <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/admin/reports" element={<AdminReports />} />
         <Route path="/login" element={<Login />} />
       </Routes>
     </BrowserRouter>
   );
 }
+
+export default App;
